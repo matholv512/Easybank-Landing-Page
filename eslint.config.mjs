10f4c050c789cfe495ca36439235acd6a924cae8
Matheus <matholv512@gmail.com>
2025-08-08 19:48:19 -0300

    chore: initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,6 @@
+import { FlatCompat } from '@eslint/eslintrc'
+import rocketseat from '@rocketseat/eslint-config/react.js'
+
+const compat = new FlatCompat()
+
+export default compat.config(rocketseat)
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-6xl text-red-400">hello world</h1>
+    </div>
+  )
+}
+
+export default App
