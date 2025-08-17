65afae872b494fb9da2b39f8a1fb35b18b60e4e2
Matheus <matholv512@gmail.com>
2025-08-17 15:47:59 -0300

    refactor(button): preserve default styles using TwMerge

--- src/components/shared/button/primary-button.tsx
+++ src/components/shared/button/primary-button.tsx
@@ -1,13 +1,22 @@
 import type { ComponentProps } from 'react'
+import { twMerge } from 'tailwind-merge'
 
 interface PrimaryButtonProps extends ComponentProps<'button'> {
   label: string
+  className?: string
 }
 
-export function PrimaryButton({ label, ...rest }: PrimaryButtonProps) {
+export function PrimaryButton({
+  label,
+  className,
+  ...rest
+}: PrimaryButtonProps) {
   return (
     <button
-      className="px-8 py-3 text-sm font-bold bg-gradient-to-r from-primary-lime-green to-primary-bright-cyan rounded-4xl hover:cursor-pointer text-neutral-light-grayish-blue hover:brightness-105 active:brightness-125"
+      className={twMerge(
+        'px-8 py-3 text-sm font-bold bg-gradient-to-r from-primary-lime-green to-primary-bright-cyan rounded-4xl hover:cursor-pointer text-neutral-light-grayish-blue hover:brightness-105 active:brightness-125',
+        className,
+      )}
       {...rest}
     >
       {label}
