919b0ba3c9aca8da7e5febd3432ca9b3eb484d8c
Matheus <matholv512@gmail.com>
2025-08-28 17:46:53 -0300

    feat(container): implement responsive paddingX prop and refactor its constants for better flexibility

--- src/components/shared/container/constants.ts
+++ src/components/shared/container/constants.ts
@@ -1,9 +1,25 @@
 export const CONTAINER = {
-  px: {
-    base: 'px-[var(--container-px-base)]',
-    sm: 'sm:px-[var(--container-px-sm)]',
-    md: 'md:px-[var(--container-px-md)]',
-    lg: 'lg:px-[var(--container-px-lg)]',
-    xl: 'xl:px-[var(--container-px-xl)]',
+  paddingX: {
+    px: {
+      base: 'px-[var(--container-px-base)]',
+      sm: 'sm:px-[var(--container-px-sm)]',
+      md: 'md:px-[var(--container-px-md)]',
+      lg: 'lg:px-[var(--container-px-lg)]',
+      xl: 'xl:px-[var(--container-px-xl)]',
+    },
+    pl: {
+      base: 'pl-[var(--container-px-base)]',
+      sm: 'sm:pl-[var(--container-px-sm)]',
+      md: 'md:pl-[var(--container-px-md)]',
+      lg: 'lg:pl-[var(--container-px-lg)]',
+      xl: 'xl:pl-[var(--container-px-xl)]',
+    },
+    pr: {
+      base: 'pr-[var(--container-px-base)]',
+      sm: 'sm:pr-[var(--container-px-sm)]',
+      md: 'md:pr-[var(--container-px-md)]',
+      lg: 'lg:pr-[var(--container-px-lg)]',
+      xl: 'xl:pr-[var(--container-px-xl)]',
+    },
   },
 } as const
--- src/components/shared/container/container.tsx
+++ src/components/shared/container/container.tsx
@@ -2,20 +2,38 @@ import type { ComponentProps, ReactNode } from 'react'
 import { twMerge } from 'tailwind-merge'
 import { CONTAINER } from './constants'
 
+type PaddingXOptions = keyof typeof CONTAINER.paddingX
+
 interface ContainerProps extends ComponentProps<'div'> {
   children: ReactNode
   className?: string
-  padding?: string
+  paddingX?: {
+    base: PaddingXOptions
+    sm?: PaddingXOptions
+    md?: PaddingXOptions
+    lg?: PaddingXOptions
+    xl?: PaddingXOptions
+  }
 }
 
 export function Container({
   children,
   className,
-  padding = twMerge(...Object.values(CONTAINER.px)),
+  paddingX = { base: 'px' },
   ...rest
 }: ContainerProps) {
+  const breakpoints = ['base', 'sm', 'md', 'lg', 'xl'] as const
+
+  const paddingXClasses = breakpoints.map((bp) => {
+    const key = paddingX[bp] ?? paddingX.base
+    return CONTAINER.paddingX[key][bp]
+  })
+
   return (
-    <div className={twMerge('mx-auto w-full', padding, className)} {...rest}>
+    <div
+      className={twMerge('mx-auto w-full', paddingXClasses, className)}
+      {...rest}
+    >
       {children}
     </div>
   )
